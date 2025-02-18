e846cfc5a1f665b054bb3a90ff7673ec3d2763a9
Mokhar-20 <[mohamedmokhtar@social-nuts.com]>
2025-02-18 16:17:57 +0200

    `feat: updated list component with dynamic title and add button`

--- src/app/manage/list/list.component.ts
+++ src/app/manage/list/list.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { tablesData } from 'src/app/utilities/shared-data/list';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.scss']
 })
-export class ListComponent {
+export class ListComponent implements OnInit {
+  type: any;
+  contentTablesData: any;
+  contentData: any;
+  tableTitles: any;
+  tableColumn: any;
+  constructor(private _activatedRoute: ActivatedRoute) {
+    this._activatedRoute.params.subscribe(
+      (param) => {
+        this.type = param['type'];
+        // console.log(this.type)
+      }
+    );
 
+    this.contentTablesData = tablesData;
+    this.contentData = this.contentTablesData[this.type];
+    this.tableTitles = this.contentData?.titles;
+    this.tableColumn = this.contentData?.column;
+  }
+
+  ngOnInit(): void {
+
+  }
 }
---
+++ src/app/utilities/shared-data/list.ts
@@ -0,0 +1,14 @@
+export const tablesData = {
+    users: {
+        hasTitle: true,
+        title: 'Users',
+        hasAddButton: true,
+        add_title: 'Add User',
+    },
+    products: {
+        hasTitle: true,
+        title: 'Products',
+        hasAddButton: true,
+        add_title: 'Add Product',
+    }
+}
