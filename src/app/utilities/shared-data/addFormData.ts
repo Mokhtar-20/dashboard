d1ef710d04c9d140d2150d91ac25ec1062b77443
Mokhar-20 <[mohamedmokhtar@social-nuts.com]>
2025-02-19 17:50:21 +0200

    testing add

--- src/app/utilities/shared-data/addFormData.ts
+++ src/app/utilities/shared-data/addFormData.ts
@@ -1,8 +1,32 @@
 export const addFormFields = {
     users: [
-        
-    ],
-    products: [
-        
+        {
+            label: 'Name',
+            name: 'name',
+            type: 'text',
+            value: '',
+            required: true,
+        },
+        {
+            label: 'Email',
+            name: 'email',
+            type: 'text',
+            value: '',
+            required: true,
+        },
+        {
+            label: 'Phone',
+            name: 'phone',
+            type: 'text',
+            value: '',
+            required: true,
+        },
+        {
+            label: 'Image',
+            name: 'image',
+            type: 'file',
+            value: '',
+            required: false,
+        },
     ]
 }
